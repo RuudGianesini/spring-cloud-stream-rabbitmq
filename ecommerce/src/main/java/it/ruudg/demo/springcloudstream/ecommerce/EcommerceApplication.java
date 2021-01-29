dd2c15dc9eb96d9e1367cb1a594dd00fd427db27
Ruud Gianesini <ruud.gianesini@iopenway.it>
2021-01-29 16:34:08 +0100

    application.yml - configurazione canali

--- ecommerce/src/main/java/it/ruudg/demo/springcloudstream/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/it/ruudg/demo/springcloudstream/ecommerce/EcommerceApplication.java
@@ -2,10 +2,8 @@ package it.ruudg.demo.springcloudstream.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
